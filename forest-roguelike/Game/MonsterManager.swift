8ab3b38bf6d3aa3833c57ef87e139a462a46cb91
Robert Taylor <rjt903@gmail.com>
2022-12-28 21:49:10 +0000

    Added monsterManager.swift

---
+++ forest-roguelike/Game/MonsterManager.swift
@@ -0,0 +1,16 @@
+//
+//  MonsterManager.swift
+//  forest-roguelike
+//
+//  Created by Robert on 28/12/2022.
+//
+
+import Foundation
+import SpriteKit
+import CoreData
+
+class MonsterManager {
+    
+    
+    
+}
